0c6f9f96eeeeccc34019697f49637d8b251f60c9
Ninad Gawad <ninadgawad@gmail.com>
2020-08-07 22:51:20 -0400

    Create 01_NLP_Spacy.py

---
+++ 01_NLP_Spacy.py
@@ -0,0 +1,8 @@
+# Import Spacy
+import spacy
+
+# Load English Model
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+for token in doc:
+    print(token.text, token.pos_, token.dep_)

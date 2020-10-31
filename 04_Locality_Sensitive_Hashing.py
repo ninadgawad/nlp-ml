44906fc6db07cea8f82dca8ecffcdf7c036552ea
Ninad Gawad <ninadgawad@gmail.com>
2020-10-31 12:24:02 -0400

    Update 04_Locality_Sensitive_Hashing.py

--- 04_Locality_Sensitive_Hashing.py
+++ 04_Locality_Sensitive_Hashing.py
@@ -6,3 +6,12 @@ def vector_side_plane(P,v):
     dotProduct = np.dot(P,v.T)
     signOfDotProduct = np.sign(dotProduct)
     return np.asscalar(signOfDotProduct)
+
+def hash_plane(P_1,v):
+    hash_value = 0 
+    for i, P in enumerate(P_1):
+        sign = vector_side_plane(P_1)
+        hash_i = 1 if sign >= 0 else 0
+        hash_value += 2**i * hash_i
+    
+    return hash_value

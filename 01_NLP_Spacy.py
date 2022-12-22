41e78cc70df41f5af5276cb4a931aab693ced6a5
Ninad Gawad <ninadgawad@gmail.com>
2022-12-22 11:19:33 -0500

    Update 01_NLP_Spacy.py

--- 01_NLP_Spacy.py
+++ 01_NLP_Spacy.py
@@ -6,3 +6,5 @@ nlp = spacy.load("en_core_web_sm")
 doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
 for token in doc:
     print(token.text, token.pos_, token.dep_)
+
+print("End")    
